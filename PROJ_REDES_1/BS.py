d7ead8f813d7045b3a3ca5271fd778a0124256c6
menapedreira <mena.pedreira@gmail.com>
2018-09-29 11:22:44 +0100

    Update BS.py

--- PROJ_REDES_1/BS.py
+++ PROJ_REDES_1/BS.py
@@ -40,19 +40,32 @@ def handlerRGR(status):
 	
 
 def parse():
-
+	current_user = ''
+	current_pass = ''
 	create_backup_server()
-	# Create a UDP socket
+	# Create a UDP socket (CS)
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	server_address = (CSNAME, CSPORT)
 	message = 'REG ' + str(BSPORT) + CSNAME
 	sock.sendto(message, server_address)
 
+	# Create a TCP socket (user)
+	sockUser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sockUser.bind(("", PORT))
+	sockUser.listen(1)
+	connUser, addrUser = sockUser.accept()
+
 	while 1:
-		
+		#messages from CS
 		data,addr = sock.recvfrom(1024)
 		print data
 		command = data.split()
+
+		#messages from users
+		dataUser,addrUser = sockUser.recvfrom(1024)
+		print dataUser
+		requestUser = dataUser.split()
+
 		if(command[0] == "RGR"):
 			handlerRGR(command[1])
 		elif(command[0] == "UAR"):
@@ -72,8 +85,36 @@ def parse():
 			user = command[1]
 			dir = command[2]
 			handlerDLB(user,dir)
+		if (requestUser[0] == 'AUT'):
+			us = str(requestUser[1])
+			pa = str(requestUser[2])
+			if(users.has_key(str(username))):
+				if(users[str(username)] == str(password)):
+					current_user = str(username)
+					current_pass = str(password)
+					connUser.sendall("AUR OK")
+					print 'User: '+str(username)
+		elif (requestUser[0] == 'UPL'):
+			file_name = requestUser[1]
+			num_files = requestUser[2]
+			l = len(requestUser)
+			i = 3
+			j = 3
+			bk = ''
+			backup = ''
+			while j < l:
+				#str stores message to print in BS (file name and num bytes received)
+				if (j % 3 == 0):
+					bk = bk + str(requestUser[i]) + requestUser[i+3] + 'Bytes received\n'
+					i = i + 4
+				#stores file to backup in file
+				backup = backup + requestUser[j]
+				j = j + 1
+			print 'RC: ' + bk
+			connUser.sendall('UPR OK')
         else:
 			print "ERR"
 			sock.close()
+			sockUser.close()
 
 parse()
